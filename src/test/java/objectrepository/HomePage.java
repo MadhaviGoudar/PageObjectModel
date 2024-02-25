2f8b36708ccfb6461d45056c25f1cf6989b68cde
MadhaviGoudar <161033198+MadhaviGoudar@users.noreply.github.com>
2024-02-25 16:20:15 +0530

    Update HomePage.java Test

--- src/test/java/objectrepository/HomePage.java
+++ src/test/java/objectrepository/HomePage.java
@@ -3,7 +3,9 @@ package objectrepository;
 public class HomePage {
 	public void  homePageClic()
 	{
-	System.out.println("This commit is by another Automation Test Engineer");	
+	System.out.println("This commit is by another Automation Test Engineer");
+
+	System.out.println("This commit is by another Automation Test Engineer 2");
 	}
 
 }
